b4159451cb744437524b3d93c13c1991021e9cd8
João Pedro <joaoplay16@gmail.com>
2022-08-08 20:41:45 -0300

    adview FULL_BANNER added

--- app/src/main/java/com/playlab/admobapp/MainActivity.kt
+++ app/src/main/java/com/playlab/admobapp/MainActivity.kt
@@ -2,10 +2,53 @@ package com.playlab.admobapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.android.gms.ads.AdListener
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.LoadAdError
+import com.playlab.admobapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        loadAd(binding.adView)
+    }
+
+    private fun loadAd(adView: AdView){
+        adView.loadAd(AdRequest.Builder().build())
+
+        adView.adListener = object: AdListener(){
+            val TAG = "MYADMOB"
+            override fun onAdLoaded() {
+                Log.d(TAG, "onAdLoaded")
+            }
+
+            override fun onAdFailedToLoad(p0: LoadAdError) {
+                Log.d(TAG, "onAdFailedToLoad")
+
+            }
+
+            override fun onAdOpened() {
+                Log.d(TAG,"onAdOpened")
+
+            }
+
+            override fun onAdClicked() {
+                Log.d(TAG, "onAdClicked")
+            }
+
+            override fun onAdClosed() {
+                Log.d(TAG, "onAdClosed")
+            }
+
+            override fun onAdImpression() {
+                Log.d(TAG, "onAdImpression")
+            }
+        }
     }
 }
